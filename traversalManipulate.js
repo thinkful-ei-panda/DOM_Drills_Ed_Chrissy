968310fa2ea14724e426fbc5e9b9993a1f462d43
A <jesus.e.estrada@hotmail.com>
2020-06-02 13:05:30 -0500

    added function that handles thumbnails

--- traversalManipulate.js
+++ traversalManipulate.js
@@ -4,6 +4,38 @@
 //   console.log(e.target);
 // });
 
-$('.thumbnail').on('click', e => {
-  $('.hero').appendTo($())
-});
+//when a user selects one of teh thumbnail images
+
+//selector for the thumbnail iamge: $('.thumbnail img')
+
+//selector for top image $('.hero img')
+
+
+function handleThumbnailClicks() {
+  //that image should be displayed in teh full-size image container at the top
+  $('.thumbnail').click(event => {
+    event.preventDefault();
+    const thumbSrc = $(event.currentTarget).find('img').attr('src');
+    $('.hero img').attr('src', thumbSrc);
+
+    const thumbAlt = $(event.currentTarget).find('img').attr('alt');
+    $('.hero img').attr('alt', thumbAlt);
+  });
+}
+
+$(handleThumbnailClicks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
